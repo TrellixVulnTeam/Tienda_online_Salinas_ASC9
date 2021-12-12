40c41cd3fc301b1885d57cf52fa25a28db0480ad
ezequielsb <ezequiel.salinasb@gmail.com>
2021-12-12 09:03:26 -0300

    prueba

---
+++ js/index.js
@@ -0,0 +1,27 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.distribucionTarjetas', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '150px'
+    })
+
+    sr.reveal ('.contenedorTarjetasCombos4', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '150px'
+    })
+
+    sr.reveal ('.contenedorTarjetasDe4', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '150px'
+    })
+    
+    sr.reveal ('.contenedorPreguntas', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '150px'
+    })
+    
+
